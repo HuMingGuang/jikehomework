ca8ad13b5abf9624bdfb0df00ae8859fed7b502f
sunshine21 <936068345@qq.com>
2021-07-18 23:38:00 +0800

    #comment 第四周作业

--- 作业目录/week04/HomeWork03/HomeWork03/HomeWork03_8.java
+++ 作业目录/week04/HomeWork03/HomeWork03/HomeWork03_8.java
@@ -8,21 +8,20 @@ import java.util.concurrent.*;
  * 方式八：采用CountDownLatch先让祝线程阻塞，等待子线程执行出结果
  */
 public class HomeWork03_8 {
-
+    private static  int result ;
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        Map<String,Integer> resultMap = new ConcurrentHashMap<>();
         long start = System.currentTimeMillis();
         CountDownLatch countDownLatch = new CountDownLatch(1);
         Runnable task = new Runnable() {
             @Override
             public void run() {
-                resultMap.put("result",fibo(36));
+                result =fibo(36);
                 countDownLatch.countDown();
             }
         };
         new Thread(task).start();
         countDownLatch.await();
-        System.out.println("异步计算结果为：" + resultMap.get("result"));
+        System.out.println("异步计算结果为：" + result);
         System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
 
     }
